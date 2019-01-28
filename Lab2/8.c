fad5a3e0db59438b0dbf2510bc6454ac5e28aaf7
Varun Gumma <45076943+Kudo-kun@users.noreply.github.com>
2019-01-28 19:45:42 +0530

    Update 8.c

--- Lab2/8.c
+++ Lab2/8.c
@@ -6,51 +6,49 @@
 #include<limits.h>
 #define N 50
 
-int n, t, count;
+int n, t, count = 0;
 /******************************************************************************/
-void DFS(int x, int dist, int visited[], int adj[][n])
+void DFS(int init, int x, int dist, int visited[], int adj[][n])
 {
-	if(!visited[x])
-	{
-		if(dist == t) 
-			count++;
-		
-		else
-			for(int j = 0; j < n; j++)
-				if(adj[x][j])
-				{ 
-					visited[x] = 1;
-					DFS(j, dist+1, visited, adj); 
-				}
-	}
+	if(dist == (t+1))																
+		count++; 
+	
+	else
+		for(int j = 0; j < n; j++)
+			if(adj[x][j] && !visited[j])
+			{ 
+				visited[x] = 1;
+				DFS(init, j, (dist + 1), visited, adj); 
+			}
+	
 	visited[x] = 0; 
 	return;
 }
-
 /******************************************************************************/
 
 int main()
 {   
 	scanf("%d", &n);
 	int adj[n][n], visited[n];
-	memset(visited, 0, n*sizeof(int));
+	memset(adj, 0, n*n*sizeof(int));
 
 	for(int i = 0, x, y; i < n; i++)
 	{
 		scanf("%d%d", &x, &y);
-		adj[x][y] = 1; adj[y][x] = 1;
+		adj[x][y] = 1; 
+		adj[y][x] = 1;
 	}
-
+	
 	scanf("%d", &t);
 	for(int i = 0; i < n; i++)
 		for(int j = 0; j < n; j++)
 			if(adj[i][j])
 			{
+				memset(visited, 0, n*sizeof(int));
 				visited[i] = 1;
-				DFS(j, 1, visited, adj);
-				visited[i] = 0;
+				DFS(i, j, 1, visited, adj);
 			}
 
-	printf("\nNumber of nodes at 't' distance from each other are: %d\n", (count >> 1));
+	printf("\nNumber of nodes which are %d units from each other are: %d\n", t, count);
     return 0;
 }
