41a2b3df5f2f27c9fed76903e15e74494662b892
Varun Gumma <varun230999@gmail.com>
2019-03-04 23:24:16 +0530

    updated

---
+++ Lab7/7.cpp
@@ -0,0 +1,41 @@
+//Author: Kudo_kun
+//302B
+#include<bits/stdc++.h>
+using namespace std;
+#define FastIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl '\n'
+#define ll long long int
+
+const int N = 105;
+int n, ans;
+pair<int, int> pa[N];
+
+int find(int k, int arr[])
+{
+    for(int i = 0; i < n; i++)
+        if(arr[i] == k)
+            return i;
+    
+    return -1;
+}
+
+
+int main()
+{
+    cin >> n;
+    int a[n], b[n];
+    for(int i = 0; i < n; i++)
+        cin >> a[i], b[i] = a[i];
+
+    sort(b, b+n);
+    for(int i = 0; i < n; i++)
+        pa[i] = {i, find(a[i], b)};
+
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            if((pa[i].first < pa[j].first) && pa[i].second > pa[j].second)
+                ans++, printf("%d %d %d %d\n", pa[i].first, pa[i].second, pa[j].first, pa[j].second);
+
+    cout << (ans/2) << endl;
+    return 0;
+}
