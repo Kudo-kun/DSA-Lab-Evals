71188dc56c0da86f2c2a94aa372b0774dd9b8a1e
Varun Gumma <varun230999@gmail.com>
2019-04-25 18:39:46 +0530

    major rewrite

--- Lab11/2.c
+++ Lab11/2.c
@@ -7,51 +7,30 @@
 #define max(a, b) ((a > b) ? a : b)
 #define min(a, b) ((a < b) ? a : b)
 
-char mag[N], note[N];
-int n, m, cnt1, cnt2;
-char words1[N][N], words2[N][N];
+char mag[N][N], note[N][N];
+int n, m, arr[N];
 /******************************************************************/
-void getWords(char* s, char X[N][N], int* cnt)
-{
-	int lo = 0, hi = 0;
-	while(lo < (strlen(s)-1))
-	{
-		while(hi < (strlen(s)-1) && s[hi] != ' ')
-			hi++;
-		printf("hello :: %d %d\n", lo, hi);
-		for(int j = lo; j < hi; j++)
-			X[*cnt][j-lo] = s[j];
-		
-		(*cnt)++;
-		lo = ++hi;
-	}
-}
-
-int check(char* s)
+int check(int u)
 {
 	for(int i = 0; i < n; i++)
-		if(!strcmp(s, words1[i]))
+		if(!strcmp(note[u], mag[i]) && !arr[i])
 		{
-			printf("found at :: %d\n", i);
+			arr[i] = 1;
 			return 1;
 		}
-
-	return -1;
+	return 0;
 }
 /******************************************************************/
 
 int main()
 {	
 	scanf("%d%d\n", &n, &m);
-	fgets(mag, N, stdin);
-	fgets(note, N, stdin);
-	
-	getWords(mag, words1, &cnt1);
-	getWords(note, words2, &cnt2);
-	if(m > n)
-		return printf("NO\n"), 0;
+	for(int i = 0; i < n; i++)
+		scanf("%s", mag[i]);
 	for(int i = 0; i < m; i++)
-		if(check(words2[i]) == -1)
+		scanf("%s", note[i]);
+	for(int i = 0; i < m; i++)
+		if(!check(i))
 			return printf("NO\n"), 0;
 
 	printf("YES\n");
